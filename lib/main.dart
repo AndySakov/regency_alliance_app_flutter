01fe2c0701fd39a9a23bf20002854bb7bba24dca
Obafemi Teminife <andysakov1958@gmail.com>
2023-03-14 14:13:04 +0100

    fix(main): fixed dex conflix

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_webview_pro/webview_flutter.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
   runApp(const MyApp());
